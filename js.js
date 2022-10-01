9117932134f2657a3b2d6063267e878acfc528fd
Stanislav <Arcanalord007@gmail.com>
2022-10-02 02:05:18 +0300

    Before opacity changes

--- js.js
+++ js.js
@@ -3,11 +3,15 @@ const input = document.querySelector('.input');
 const submit = document.querySelector('#button-submit');
 const reset = document.querySelector('#button-reset');
 const color = document.querySelector('#fav-color');
-
+const rainbow = document.querySelector('#rainbow');
+const shadowing = document.querySelector('#shadowing')
 const SQUARE = 640000;
+let rainbowModCheck = false;
+let strengthModCheck = false;
 
-
-
+let colorHere = color.addEventListener('input', takeColor)
+let rainbowMod = rainbow.addEventListener('change', makeRainbow);
+let strength = shadowing.addEventListener('change', makeStrength);
 
 defaultGrid();
 
@@ -21,7 +25,10 @@ function defaultGrid() {
     listenAfterSizeChanged();
 }
 
-submit.addEventListener('click', gridMaker);
+submit.addEventListener('click', e => {
+    e.preventDefault(); 
+    gridMaker();
+    });
 
 function gridMaker() {
     let input = document.getElementById('textbox');
@@ -29,7 +36,7 @@ function gridMaker() {
     if (inputInt < 100 && 0 < inputInt) {
             resizeField(inputInt);
     } else {
-    defaultGrid();  
+        resizeField(16);
     }
 }
 
@@ -53,22 +60,72 @@ function listenAfterSizeChanged () {
 const pixel = document.querySelectorAll('.pixel');
 pixel.forEach(e => e.addEventListener('mouseover', changeColor));
 function changeColor(e) {
-    e.target.style.backgroundColor = `${takeColor()}`;
-    console.log(e.target)
+    if (strengthModCheck) {
+        e.target.style.backgroundColor = 'blue';
+    } else {
+        e.target.style.backgroundColor = setFinalColor();
+    // console.log(e.target)
+    }
     }
 }
 
 reset.addEventListener('click', resetFilledBoxes);
 
-function resetFilledBoxes() {
-    console.log('hi')
+function resetFilledBoxes(e) {
+    e.preventDefault();
+    console.log('hihi')
     gridMaker();
 }
 
-let colorHere = color.addEventListener('input', takeColor)
+
 
 function takeColor() {
     console.log(color.value)
     return color.value;
 }
 
+
+function makeRainbow() {
+    
+    if (this.checked) {
+        rainbowModCheck = true; 
+    } else {
+        rainbowModCheck = false;
+    }
+}
+
+function makeStrength() {
+    if (this.checked) {
+        strengthModCheck = true;
+        rainbow.disabled = true;
+    } else {
+        strengthModCheck = false;
+        rainbow.disabled = false;
+    }
+}
+
+function setFinalColor() {
+    let color = '';
+    if (rainbowModCheck && !strengthModCheck) {
+        color = '#' + (Math.random()*0xFFFFFF<<0).toString(16);
+        return color;
+    } else if (strengthModCheck) {
+        color = addAlpha(`${takeColor()}`);
+        console.log(color)
+        return color;
+    } else {
+        color = takeColor();
+        return color;
+    }
+}
+
+function addAlpha(color) {
+    console.log(color.length)
+    // if (color.length)
+    let sum = 0;
+    let tenPercent = Math.round(255 * 0.1);
+    console.log(color + tenPercent.toString(16));
+    sum += tenPercent;
+    return color + tenPercent.toString(16);
+
+}
